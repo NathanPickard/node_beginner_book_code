06eb1b9e71952e28e500b582bcee003201088c30
Nathan Pickard <nathan.pickard87@gmail.com>
2016-11-27 10:17:26 -0800

    Continuing exercises

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,28 +1,40 @@
-var exec = require("child_process").exec;
-
-function start() {
+function start(response) {
   console.log("Request handler 'start' was called.");
-  var content = "empty";
 
-  exec("ls -lah", function (error, stdout, stderr) {
-    content = stdout;
-  });
-
-  return content;
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
 }
 
-//   function sleep(milliSeconds) {
-//     var startTime = new Date().getTime();
-//     while (new Date().getTime() < startTime + milliSeconds);
-//   }
+// var exec = require("child_process").exec;
 
-//   sleep(10000);
-//   return "Hello Start";
+// function start(response) {
+//   console.log("Request handler 'start' was called.");
+  
+//   exec("ls -lah", function (error, stdout, stderr) {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write(stdout);
+//     response.end();
+//   });
 // }
 
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called.");
-  return "Hello Upload";
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-// var http = require("http");
-
-// http.createServer(function(request, response) {
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.write("Hello World");
-//     response.end();
-// }).listen(8888);
-
 var http = require("http");
 var url = require("url");
 
@@ -14,11 +6,14 @@ function start(route, handle) {
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
 
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        var content = route(handle, pathname)
-        response.write(content);
-        response.end();
+        route(handle, pathname, response);
     }
+
+    //     response.writeHead(200, {"Content-Type": "text/plain"});
+    //     var content = route(handle, pathname)
+    //     response.write(content);
+    //     response.end();
+    // }
 
     http.createServer(onRequest).listen(8888);
     console.log("Server has started.");

12f26bda92534b25511daecbfe02bbc5a31d4d71
Nathan Pickard <nathan.pickard87@gmail.com>
2016-11-30 15:23:23 -0800

    Completed the coding project

--- requestHandlers.js
+++ requestHandlers.js
@@ -46,10 +46,10 @@ function upload(response, request) {
 
     /* Possible error on Windows systems:
         tried to rename to an already existing file */
-        fs.rename(files.upload.path, "/tmp/test.png", function(error) {
+        fs.rename(files.upload.path, "./tmp/test.png", function(error) {
           if (error) {
             fs.unlink("/tmp/test.png");
-            fs.rename(files.upload.path, "/tmp/test.png");
+            fs.rename(files.upload.path, "./tmp/test.png");
           }
         });
 
